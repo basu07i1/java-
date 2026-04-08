9ead5bf6c15416c8288a3bff97c409852093a806
basu07i1 <basantbasu1078@gmail.com>
2026-04-08 16:45:13 +0530

    new ques

--- Practice_Pattern.java
+++ Practice_Pattern.java
@@ -91,23 +91,23 @@ public class Practice_Pattern {
             System.out.println();
             
         }
-        // 2half 
-        // for(int i=n; i>=1; i--){
-        //     //stars
-        //     for(int j=1; j<=i; j++){
-        //         System.out.print("*");
-        //     }
-        //     //space
-        //     for(int j=1; j<=2*(n-i); j++){
-        //         System.out.print(" ");
-        //     }
+        //2half 
+        for(int i=n; i>=1; i--){
+            //stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            //space
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
 
-        //     //start
-        //     for(int j =1; j<=i; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+            //start
+            for(int j =1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
         public static void main(String args[]) {
 
