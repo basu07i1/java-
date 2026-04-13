3df25dba170b7aa0d372495d7efd57a3e44de677
basu07i1 <basantbasu1078@gmail.com>
2026-04-13 07:05:43 +0530

    pair array

---
+++ Array_Pair.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Array_Pair {
+    public static void main(String[] args){
+        int[] arr = {4, 9, 10, 7, 5};
+
+        for(int i=0; i<arr.length; i++){
+            int first = arr[i];
+            for(int j=i+1; j<arr.length; j++){
+                int second = arr[j];
+                int sum = first +second;
+                System.out.println("Making pair of " + first + " and " + second + " sum: " + sum);
+            }
+        }
+    }
+    
+}
