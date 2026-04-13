71a01e6b1a5f091b15678c6197fb9005d50a03c1
basu07i1 <basantbasu1078@gmail.com>
2026-04-13 07:13:46 +0530

    Update Array_Pair and other Java files

--- Function_Prime.java
+++ Function_Prime.java
@@ -1,15 +1,6 @@
 //Check if a number is Prime or not ;
 public class Function_Prime{
-    // public static boolean isPrime(int n){
-    //     boolean isPrime = true;
-    //     for(int i = 2; i<=n-1; i++){
-    //         if(n % i == 0){
-    //             isPrime = false;
-    //             break;
-    //         }
-    //     }
-    //     return isPrime;
-    // }
+    // 
 
     //Optimized way:-
     public static boolean isPrime(int n){
