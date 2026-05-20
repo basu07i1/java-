93743c57b47dfc20d7b43dca0ebb99029c175be3
basu07i1 <basantbasu1078@gmail.com>
2026-05-20 14:05:40 +0530

    sorting

--- BasicSorting.java
+++ BasicSorting.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class BasicSorting {
     public static void bubbleSort(int arr[]) {
         for(int turn = 0; turn < arr.length-1; turn++) {
-            for(int j=0; j<=arr.length-1-turn; j++){
+            for(int j=0; j<=arr.length-2-turn; j++){
                 if(arr[j] > arr[j+1]){
                     //swap
                     int temp = arr[j];
@@ -21,12 +21,49 @@ public class BasicSorting {
         }
         System.out.println();
     }
+
+    public static void selectionSort(int arr[]){
+        for(int i=0; i<arr.length-1; i++){ //outer loop
+            int minPosition = i; // this is current position .
+            for( int j=i+1; j < arr.length; j++){
+                if(arr[minPosition] > arr[j]){
+                    minPosition = j;
+                }
+            }
+            //swap 
+            int temp = arr[minPosition];
+            arr[minPosition] = arr[i];
+            arr[i] = temp;
+        }
+
+    }
+
+    public static void insertionSort(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev = i-1;
+
+            //find out the current position to insert
+            while(prev >= 0; && arr[prev] > curr){
+                arr[prev + 1] = arr[prev];
+
+                prev--;
+                
+            }
+            // insertion
+            arr[prev +1] = arr[curr]; 
+
+        }
+    }
     
 
     public static void main(String args[]){
         int arr[] = {5 , 4 , 1 , 3, 2};
-        bubbleSort(arr);
+        // bubbleSort(arr);
+        // selectionSort(arr);
+        insertionSort(arr);
         printArr(arr);
+        
     }
     
 }
