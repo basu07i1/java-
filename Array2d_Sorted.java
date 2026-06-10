5f0f1ce0de5d93ca4963c5ac32f4d2391a604cdd
basu07i1 <basantbasu1078@gmail.com>
2026-06-10 08:53:39 +0530

    2d

---
+++ Array2d_Sorted.java
@@ -0,0 +1,37 @@
+public class Array2d_Sorted {
+
+    public static boolean staircaseSearch(int[][] matrix, int key) {
+
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+
+            if (matrix[row][col] == key) {
+                return true;
+            }
+            else if (matrix[row][col] > key) {
+                col--;
+            }
+            else {
+                row++;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {10, 20, 30, 40},
+            {15, 25, 35, 45},
+            {27, 29, 37, 48},
+            {32, 33, 39, 50}
+        };
+
+        int key = 133;
+
+        System.out.println(staircaseSearch(matrix, key));
+    }
+}
